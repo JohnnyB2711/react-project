d09a9c159d84b605c938a6ce836be17c0df8b770
Евгения <eugenebeklemisheva2711@gmail.com>
2019-09-03 17:50:49 +0300

    Menu,немного Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header/Header'
-import Content from './components/Contetn'
-import Menu from './components/Menu'
+import Content from './components/Content/Contetn'
+import Menu from './components/Menu/Menu'
 import 'bootstrap/dist/css/bootstrap.css'
 function App() {
     return(
