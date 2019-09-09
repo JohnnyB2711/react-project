7cea01cf8a608beda6a36c90b54cd3ded4b57c38
Евгения <eugenebeklemisheva2711@gmail.com>
2019-09-09 17:08:12 +0300

    переделать на компонент Pagination почему не получается обращаться к film в компоненте MovieCard

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -3,53 +3,29 @@ import {Card, NavDropdown} from 'react-bootstrap';
 import './MovieCard.scss'
 import '../Buttons/Buttons'
 import Buttons from "../Buttons/Buttons";
+import Toprated from "../../layouts/Toprated/Toprated";
 import axios from "axios";
 import Store from "../../stores";
 
 const API_KEY = "ac24c5f255eb805f019fbfdd3539c068";
 
 class MovieCard extends React.Component {
-    state = {
-        films: []
-    }
-
-    async componentDidMount() {
-        this.setState({
-            films: this.getFilms()
-        })
-    }
-
-    getFilms = async () => {
-        try {
-            const {data} = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`);
-            return data
-        } catch {
-            console.log('error')
-        }
-    };
-
     render() {
         return (
-            this.state.films.map((item) => {
-                return (
-                    <Card className='Card' key={item.id}>
-                        <Card.Img variant='top' src={`${item.poster_path}`}></Card.Img>
-                        <Card.Body>
 
-                            <Card.Title>{}</Card.Title>
-                            <Card.Subtitle>Жанр</Card.Subtitle>
-                            <Buttons/>
+            <Card className='Card' key={this.props.film}>
+                <Card.Img variant='top' src={this.props.poster}></Card.Img>
+                <Card.Body>
 
-                        </Card.Body>
-                    </Card>
-                )
+                    <Card.Title>{this.props.title}</Card.Title>
+                    <Card.Subtitle>{this.props.genre_id}</Card.Subtitle>
+                    <Buttons/>
 
-                return <NavDropdown.Item key={item.id} href={`/genre/${item.id}`}>{item.name}</NavDropdown.Item>
-            })
-
-
-        )
-    }
+                </Card.Body>
+            </Card>
+            //<NavDropdown.Item key={item.id} href={`/genre/${item.id}`}>{item.name}</NavDropdown.Item>
+    )
+}
 
 
 }
--- src/layouts/Toprated/Toprated.js
+++ src/layouts/Toprated/Toprated.js
@@ -3,30 +3,83 @@ import MovieCard from "../../components/MovieCard/MovieCard";
 import '../Layouts.scss'
 import Dispatcher from "flux";
 import axios from "axios";
+import {ButtonGroup, ButtonToolbar, Button, NavDropdown, Pagination, PageItem} from 'react-bootstrap'
+
 const API_KEY = "ac24c5f255eb805f019fbfdd3539c068";
 
-class Toprated extends React.Component{
+class Toprated extends React.Component {
+    state = {
+        films: [],
+        page: 1,
+        total_pages: 0,
+        filmPage: 20
+    };
 
-    /*async componentDidMount() {
-        this.getFilms()
+    async componentDidMount() {
+        this.getFilms(this.page)
     }
-    getFilms = async () => {
+
+    getFilms = async (page_number) => {
         try {
-            const {data} = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`);
-            console.log(data)
+            const {data} = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=${page_number}`);
+            console.log(data.results)
+            await this.setState({
+                films: data.results,
+                total_pages: data.total_pages
+            })
         } catch {
             console.log('error')
         }
-    };*/
+    };
+
     render() {
+        let pages = [];
+        for (let i = 1; i <= this.state.total_pages; i++) {
+            pages.push(i)
+        }
         return (
             <div className='Body container-fluid'>
                 <h1>Top rated films</h1>
                 <div className='row'>
-                    <div className='col-md-3'>
-                        <MovieCard/>
-                    </div>
+                    {
+                        this.state.films.map((film) => {
+                            return <div key={film.id} className='col-md-3'>
+                                <MovieCard film={film}/>
+                            </div>
+                        })
+                    }
                 </div>
+                <Pagination>
+                    <Pagination.First/>
+                    <Pagination.Prev/>
+                    <Pagination.Item>{1}</Pagination.Item>
+                    <Pagination.Ellipsis/>
+
+                    <Pagination.Item>{10}</Pagination.Item>
+                    <Pagination.Item>{11}</Pagination.Item>
+                    <Pagination.Item>{12}</Pagination.Item>
+                    <Pagination.Item>{13}</Pagination.Item>
+                    <Pagination.Item disabled>{14}</Pagination.Item>
+
+                    <Pagination.Ellipsis/>
+                    <Pagination.Item>{pages}</Pagination.Item>
+                    <Pagination.Next/>
+                    <Pagination.Last/>
+                </Pagination>
+
+                    <ButtonToolbar>
+                        <ButtonGroup>
+                            {
+                                pages.map((page_number) => {
+                                    return <Button key={page_number} onClick={(e) => {
+                                        this.getFilms(page_number)
+                                    }}>{page_number}</Button>
+                                })
+                            }
+                        </ButtonGroup>
+
+                    </ButtonToolbar>
+
 
             </div>
         )
